61bd128d2347f75617e87a17f4c98d350885c079
Mykola Golub <mgolub@mirantis.com>
2016-10-31 22:02:55 +0200

    fixup

--- src/test/librbd/test_librbd.cc
+++ src/test/librbd/test_librbd.cc
@@ -4984,7 +4984,7 @@ TEST_F(TestLibRBD, ExclusiveLock)
 TEST_F(TestLibRBD, DiscardAfterWrite)
 {
   CephContext* cct = reinterpret_cast<CephContext*>(_rados.cct());
-  REQUIRE(!cct->_conf->rbd_skip_partial_discard)'
+  REQUIRE(!cct->_conf->rbd_skip_partial_discard);
 
   librados::IoCtx ioctx;
   ASSERT_EQ(0, _rados.ioctx_create(m_pool_name.c_str(), ioctx));
